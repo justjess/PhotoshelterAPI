71026a87c097ae8f24f04c4121f6edea2da7a601
Ulhas Vardhan Golchha <golchha21@gmail.com>
2013-04-09 00:31:44 +0530

    Minor CSS & HTML updates

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
 <html lang="en">
 
 	<head>
-		<meta charset="utf-8">
 		<title><?php echo _CODENAME; ?> v <?php echo _VERSION; ?></title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="">
