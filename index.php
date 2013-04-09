935cb38bb1859532aa7ea6eba3d68f5f20c32ed3
Ulhas Vardhan Golchha <golchha21@gmail.com>
2013-04-10 00:55:51 +0530

    Updated with social tracker & analytics

--- index.php
+++ index.php
@@ -14,12 +14,14 @@
 		<meta name="keywords" content="Photoshelter, PhotoshelterAPI, GitHub Project, Ulhas Vardhan Golchha">
 		<meta name="description" content="Unofficial PhotoshelterAPI for putting up the gallery on your non Photoshelter websites.">
 		<meta name="author" content="Ulhas Vardhan Golchha">
+		<link href="https://plus.google.com/100459034318170995980/" rel="publisher">
+		<meta property="fb:admins" content="619145803">
 		<!--[if lt IE 9]>
 		<script src="https://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
-		<link rel="stylesheet" href="css/style.css.php" type="text/css" />
-		<link href="https://plus.google.com/100459034318170995980/" rel="publisher">
-		<meta property="fb:admins" content="619145803">
+		<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
+		<link rel="stylesheet" href="css/bootstrap-responsive.min.css" type="text/css" />
+		<link rel="stylesheet" href="css/bootstrap-extended.min.css" type="text/css" />	
 	</head>
 
 	<body data-spy="scroll">
@@ -37,7 +39,7 @@
 						<ul class="nav pull-right" id="main-menu-rights">
 							<li><a href="http://www.technologyunited.in/" target="_blank" onclick="_gaq.push(['_trackEvent', 'Navigation actions', 'Navigation links', 'Technology United']);">Technology United</a></li>
 							<li><a href="http://www.ulhas.net/" target="_blank" onclick="_gaq.push(['_trackEvent', 'Navigation actions', 'Navigation links', 'Ulhas Vardhan Golchha']);">Ulhas Vardhan Golchha</a></li>
-							<li><a href="https://github.com/golchha21/PhotoshelterAPI/issues" target="_blank" onclick="_gaq.push(['_trackEvent', 'Navigation actions', 'Navigation links', 'Photoshelter Feedback/Issues']);">Feedback/Issues</a></li>
+							<li><a href="<?php echo _URL; ?>/issues" target="_blank" onclick="_gaq.push(['_trackEvent', 'Navigation actions', 'Navigation links', 'Photoshelter Feedback/Issues']);">Feedback/Issues</a></li>
 						</ul>
 					</div>
 				</div>
@@ -70,15 +72,44 @@
 
 			<footer class="footer">
 				<div class="row">
+					<div class="span12">
+						<div class="social">
+							<ul class="social-buttons">
+							  <li>
+								<iframe class="github-btn" src="http://ghbtns.com/github-btn.html?user=golchha21&amp;repo=PhotoshelterAPI&amp;type=watch&amp;count=true" allowtransparency="true" frameborder="0" height="20px" scrolling="0" width="100px"></iframe>
+							  </li>
+							  <li>
+								<iframe class="github-btn" src="http://ghbtns.com/github-btn.html?user=golchha21&amp;repo=PhotoshelterAPI&amp;type=fork&amp;count=true" allowtransparency="true" frameborder="0" height="20px" scrolling="0" width="102px"></iframe>
+							  </li>
+							  <li>
+								<iframe class="github-btn" src="http://ghbtns.com/github-btn.html?user=golchha21&type=follow&count=true" allowtransparency="true" frameborder="0" scrolling="0" width="165" height="20"></iframe>
+							  </li>
+							  <li>
+								<a href="https://twitter.com/golchha21" class="twitter-follow-button" data-link-color="#0069D6" data-show-count="true">Follow @golchha21</a>
+							  </li>
+							  <li>
+								<a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php echo _URL; ?>" data-text="Unofficial PhotoshelterAPI for putting up the gallery on your non Photoshelter websites." data-via="golchha21">Tweet</a></iframe>
+							  </li>
+							  <li>
+								<span class="g-plusone" data-size="tall" data-annotation="inline" data-width="120" data-href="<?php echo _URL; ?>"></span>
+							  </li>
+							  <li>
+								<span class="fb-like" data-href="<?php echo _URL; ?>" data-send="true" data-layout="button_count" data-width="150" data-show-faces="false"></span>
+							  </li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="row">
 					<div class="span4">
 						<div class="page-header">
 							<h4>Catch me at: </h4>
 						</div>
-						<ul id="social_icons">
-							<li id="facebook"><a data-original-title="Find us on Facebook" id="tfacebook" href="https://www.facebook.com/golchha21" rel="tooltip" target="_blank" onclick="_gaq.push(['_trackEvent', 'Footer actions', 'Social links', 'Facebook']);">Facebook<span></span></a></li>
-							<li id="twitter"><a data-original-title="Follow us on Twitter" id="ttwitter" href="https://www.twitter.com/golchha21" rel="tooltip" target="_blank" onclick="_gaq.push(['_trackEvent', 'Footer actions', 'Social links', 'Twitter']);">Twitter<span></span></a></li>     		
-							<li id="googleplus"><a data-original-title="Follow us on Google+" id="tgoogleplus" href="https://plus.google.com/100459034318170995980" rel="tooltip" target="_blank" onclick="_gaq.push(['_trackEvent', 'Footer actions', 'Social links', 'Google+']);">Google+<span></span></a></li>
-							<li id="linkedin"><a data-original-title="Follow us on LinkedIn" id="tlinkedin" href="http://www.linkedin.com/in/golchha21" rel="tooltip" target="_blank" onclick="_gaq.push(['_trackEvent', 'Footer actions', 'Social links', 'LinkedIn']);">LinkedIn+<span></span></a></li>
+						<ul class="social_icons">
+							<li class="facebook"><a data-original-title="Find us on Facebook" id="tfacebook" href="https://www.facebook.com/golchha21" rel="tooltip" target="_blank" onclick="_gaq.push(['_trackEvent', 'Footer actions', 'Social links', 'Facebook']);">Facebook<span></span></a></li>
+							<li class="twitter"><a data-original-title="Follow us on Twitter" id="ttwitter" href="https://www.twitter.com/golchha21" rel="tooltip" target="_blank" onclick="_gaq.push(['_trackEvent', 'Footer actions', 'Social links', 'Twitter']);">Twitter<span></span></a></li>
+							<li class="googleplus"><a data-original-title="Follow us on Google+" id="tgoogleplus" href="https://plus.google.com/100459034318170995980" rel="tooltip" target="_blank" onclick="_gaq.push(['_trackEvent', 'Footer actions', 'Social links', 'Google+']);">Google+<span></span></a></li>
+							<li class="linkedin"><a data-original-title="Follow us on LinkedIn" id="tlinkedin" href="http://www.linkedin.com/in/golchha21" rel="tooltip" target="_blank" onclick="_gaq.push(['_trackEvent', 'Footer actions', 'Social links', 'LinkedIn']);">LinkedIn+<span></span></a></li>
 						</ul>
 					</div>
 					<div class="span8">
@@ -91,26 +122,49 @@
 				<hr>
 				<div class="row">
 					<div class="span12">
-						<p class="copyright pull-left">Copyright &copy; 2013, <a href="http://www.technologyunited.in/" id="credittuamc" target="_blank" onclick="_gaq.push(['_trackEvent', 'Footer actions', 'Footer links', 'Copyright']);" title="Technology United">Technology United</a>. All rights reserved.</p><p class="pull-right"><a href="#top">Back to top</a></p>
+						<p class="copyright pull-left">Copyright &copy; 2013, <a href="http://www.technologyunited.in/" id="credittuamc" target="_blank" onclick="_gaq.push(['_trackEvent', 'Footer actions', 'Footer links', 'Copyright']);" title="Technology United">Technology United</a>. All rights reserved.</p>
+						<p class="pull-right"><a href="#top">Back to top</a></p>
 					</div>
 				</div>
 			</footer>
 
 		</div>
 		
-		<script type='text/javascript' src='js/javascript.js.php'></script>
-		<script type="text/javascript">
-
+		<script src='js/jquery.js'></script>
+		<script src='js/bootstrap.min.js'></script>
+		<script src='js/jquery.smooth-scroll.min.js'></script>
+		<script src='js/jquery.masonry.js'></script>
+		<script src='js/social.tracker.js'></script>
+		<script src='js/prettify.js'></script>
+		<script src='js/scripts.js'></script>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+		<script type='text/javascript'>
 		  var _gaq = _gaq || [];
 		  _gaq.push(['_setAccount', 'UA-5397392-9']);
 		  _gaq.push(['_trackPageview']);
-
+		  
 		  (function() {
 			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 		  })();
 
+		  (function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+			js = d.createElement(s); js.id = id;
+			js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=220974161291591";
+			fjs.parentNode.insertBefore(js, fjs);
+		  }(document, 'script', 'facebook-jssdk'));
+	
+		  (function() {
+			var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+			po.src = 'https://apis.google.com/js/plusone.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+		  })();
+		  
+		  _ga.trackTwitter();
+		  _ga.trackFacebook();
 		</script>
 	</body>
 </html>
