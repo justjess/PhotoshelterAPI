52b0bf5e78d69518f558b77816e8d7ce80446f66
justjess <admin@justjess.com>
2014-06-25 16:21:18 -0400

    remove GA tracking

--- index.php
+++ index.php
@@ -145,38 +145,7 @@
 		<script src='js/social.tracker.js'></script>
 		<script src='js/prettify.js'></script>
 		<script src='js/scripts.js'></script>
-		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-		<script type='text/javascript'>
-			var _gaq = _gaq || [];
-			var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
-			_gaq.push(['_require', 'inpage_linkid', pluginUrl]);
-			_gaq.push(['_setAccount', 'UA-5397392-2']);
-			_gaq.push(['_setDomainName', 'ulhas.net']);
-			_gaq.push(['_setAllowLinker', true]);
-			_gaq.push(['_trackPageview']);
-
-			(function() {
-				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-			})();
-			
-			(function(d, s, id) {
-				var js, fjs = d.getElementsByTagName(s)[0];
-				if (d.getElementById(id)) return;
-				js = d.createElement(s); js.id = id;
-				js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=220974161291591";
-				fjs.parentNode.insertBefore(js, fjs);
-			}(document, 'script', 'facebook-jssdk'));
-	
-			(function() {
-				var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-				po.src = 'https://apis.google.com/js/plusone.js';
-				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-			})();
-		  
-		  _ga.trackTwitter();
-		  _ga.trackFacebook();
-		</script>
+		
+		
 	</body>
 </html>
